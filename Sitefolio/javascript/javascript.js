346e7375bb732d4c5a9dbdca1d02d89b346a3f59
Brenda Luiza Pereira <62603515+Brendaluizapereira@users.noreply.github.com>
2020-07-20 14:46:08 -0300

    menu completo 2 stylesheets media query

--- Sitefolio/javascript/javascript.js
+++ Sitefolio/javascript/javascript.js
@@ -4,6 +4,7 @@ function myFunction() {
   var x = document.getElementById("mylinks");
  
   if (x.style.display === "block") {
+    x.style.display = "none";
   } else {
     x.style.display = "block";
     x.style.textDecoration = "none";
